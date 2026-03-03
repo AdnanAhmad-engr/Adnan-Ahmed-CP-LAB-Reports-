fead1435e1be14671fc83015ee183b0ed8876290
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-03-04 04:58:38 +0500

    Add files via upload

---
+++ CP LAB 1/LAB WORK/LAB WORK (LAB 1).cpp
@@ -0,0 +1,18 @@
+// Name: Adnan Ahmad 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+
+
+
+#include <iostream>   // library ha iostream ma 
+using namespace std;  // standard name use krny k liay add kiay han
+
+int main() 
+{
+    
+    cout << "Hello World!"; // display k liye
+
+    return 0;  // program shuru b aur khatam b krta ha
+}
