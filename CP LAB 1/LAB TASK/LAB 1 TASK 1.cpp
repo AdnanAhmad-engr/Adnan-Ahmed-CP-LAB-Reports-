d462f3cd33fc5ed47dce62bd0c95880cf775dee4
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-03-04 04:55:36 +0500

    Add files via upload

---
+++ CP LAB 1/LAB TASK/LAB 1 TASK 1.cpp
@@ -0,0 +1,18 @@
+// Name: Adnan Ahmad 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+
+
+
+
+#include <iostream> //include library for input and output
+using namespace std; // allow standard names like "cout and cin" without std:
+// function main begins program execution
+int main()// it is sttarting point of program
+    {
+
+
+
+}
