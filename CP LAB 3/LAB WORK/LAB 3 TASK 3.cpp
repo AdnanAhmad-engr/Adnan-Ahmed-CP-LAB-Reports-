8d270d09b0a262a0fa8ac810ac6f59095f46ac18
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-03-04 05:05:32 +0500

    Add files via upload

---
+++ CP LAB 3/LAB WORK/LAB 3 TASK 3.cpp
@@ -0,0 +1,23 @@
+// Name: Adnan Ahmad 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+int num1; // VARIABLE DECLERE KRNY K LYE
+
+cout<<"enter num\n";// DISPLAY K LYE 
+cin>>num1;  // USER JO INPUT DE GA 
+
+if(num1>0)//   CONDITION LAGANY K LYE 
+cout<<num1<<" is positive"<<endl;   // PRINT KRNY K LYE 
+
+if(num1<0)//   CONDITION LAGAANY K LYE 
+cout<<num1<<" is negative"<<endl;//   DISPLAY KARNY K LYE 
+return 0;
+}
