43430fe323dd12ac5a7fd70e5f3a5d833b4b8150
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-03-04 05:00:25 +0500

    Add files via upload

---
+++ CP LAB 2/LAB TASK/LAB_2 TASK_4.cpp
@@ -0,0 +1,19 @@
+// Name: Adnan Ahmad 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+
+
+#include <iostream>   
+using namespace std;  // standard nam cin aur cout use krny k liye
+
+int main()
+{
+    int a;  // variable store krny k liye
+
+    cout << "Dear user, kindly enter your age: ";  // display
+    cin >> a;  // read value in a 
+
+    return 0;  
+}
---
+++ CP LAB 2/LAB TASK/LAB_2 TASK_7.cpp
@@ -0,0 +1,20 @@
+// Name: Adnan Ahmad 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // loop to print name 4 times
+    for (int i = 1; i < 5; i++)
+    {
+        cout << "qazi ehsanullah" << endl;  // print name
+    }
+
+    return 0; 
+}
