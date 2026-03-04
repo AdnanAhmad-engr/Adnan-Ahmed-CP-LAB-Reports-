279ca3e66ca31676e0922757862a2fc7d68f5adc
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-03-04 05:02:32 +0500

    Add files via upload

---
+++ CP LAB 2/LAB WORK/LAB 2 TASK 2.cpp
@@ -0,0 +1,16 @@
+// Name: Adnan Ahmad 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	float pi; // FLAOT USE KIA HA DECIMAL VALUE K LIYE
+    	pi=3.14; // VALUE DECLARE
+    	cout<<"value of pi =\t"<<pi; // DISPLAY
+
+
+return 0;
+}
