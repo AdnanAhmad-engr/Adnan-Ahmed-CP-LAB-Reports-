df5bec2961689e5494ec0697ba22f3147c028a0a
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-03-05 23:29:25 +0500

    Add files via upload

---
+++ CP LAB 2/LAB WORK/LAB_2 WORK_3.cpp
@@ -0,0 +1,19 @@
+// Name: Adnan Ahmad 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+
+
+#include <iostream>   // library for input and output
+using namespace std;
+
+int main()
+{
+    int a = 10;  // initialize integer variable and assign value
+
+    cout << a;   // print value of a
+
+    return 0;   
+}
+
