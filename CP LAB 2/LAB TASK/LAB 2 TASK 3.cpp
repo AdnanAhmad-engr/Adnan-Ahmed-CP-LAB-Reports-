51a743479ed7531e87cb6678461eaad812ad1d98
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-03-05 23:25:29 +0500

    Add files via upload

---
+++ CP LAB 2/LAB TASK/LAB 2 TASK 3.cpp
@@ -0,0 +1,23 @@
+// Name: ADNAN AHMAD 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0744
+// Section: A
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	double F; // WORDS LIKHNY K LIYE
+    	float C; // DECIMAL VALUE K LIYE 
+    	cout<<"enter temperature in celcius ";  // DISPLAY
+    	cin>>C;
+    F = (C * 9/5) + 32;  // FORMULA CONVERRSION K LIYE
+    	cout<<F <<"  farheight";
+	
+
+return 0;
+}
