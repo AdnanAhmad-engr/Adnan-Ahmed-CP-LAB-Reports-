514511713620307a419cad145fbf5b8f48af95eb
AdnanAhmad-engr <khanadnan3989khan@gmail.com>
2026-05-14 16:16:54 +0500

    Add files via upload

---
+++ CP LAB 09/lab work/Lab_09 LabWork_02.cpp
@@ -0,0 +1,21 @@
+// Name: Adnan Ahmad
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0744
+// Section: A
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void sayHello()
+{
+    // Display message
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    // Function call
+    sayHello();
+
+    return 0;
+}
